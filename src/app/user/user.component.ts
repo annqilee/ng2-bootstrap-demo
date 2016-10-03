459e535daffaf9233f3db2ef8bf8bdd437e2feb7
zhang.xiaofei10 <zhang.xiaofei10@zte.com.cn>
2016-10-03 17:21:59 +0800

    user interfaces;

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -8,58 +8,66 @@ import { PaginationModule } from 'ng2-bootstrap/ng2-bootstrap';
   styleUrls: ['./user.component.scss']
 })
 export class UserComponent implements OnInit {
-  	public status:Object = {
-    	open: true,
-  	};
-
-  	public userTypes:any=[
-  		{id:'1',name:'userType1'},
-  		{id:'2',name:'userType2'},
-  		{id:'3',name:'userType3'},
-  		{id:'4',name:'userType4'},
-  		{id:'5',name:'userType5'},
-  		{id:'6',name:'userType6'},
-  		{id:'7',name:'userType7'},
-  		{id:'8',name:'userType8'},
-  		{id:'9',name:'userType9'},
-  		{id:'10',name:'userType10'}
-  	];
-
+    //test data
   	public users:any=[
-  		{index:1,userName:'momo',nickName:'大漠穷秋',regTime:'2016-10-02 22:31',lastLoginTime:'2016-10-02 22:31'},
-  		{index:2,userName:'momo',nickName:'大漠穷秋',regTime:'2016-10-02 22:31',lastLoginTime:'2016-10-02 22:31'},
-  		{index:3,userName:'momo',nickName:'大漠穷秋',regTime:'2016-10-02 22:31',lastLoginTime:'2016-10-02 22:31'},
-  		{index:4,userName:'momo',nickName:'大漠穷秋',regTime:'2016-10-02 22:31',lastLoginTime:'2016-10-02 22:31'},
-  		{index:5,userName:'momo',nickName:'大漠穷秋',regTime:'2016-10-02 22:31',lastLoginTime:'2016-10-02 22:31'},
-  		{index:6,userName:'momo',nickName:'大漠穷秋',regTime:'2016-10-02 22:31',lastLoginTime:'2016-10-02 22:31'},
-  		{index:7,userName:'momo',nickName:'大漠穷秋',regTime:'2016-10-02 22:31',lastLoginTime:'2016-10-02 22:31'},
-  		{index:8,userName:'momo',nickName:'大漠穷秋',regTime:'2016-10-02 22:31',lastLoginTime:'2016-10-02 22:31'},
-  		{index:9,userName:'momo',nickName:'大漠穷秋',regTime:'2016-10-02 22:31',lastLoginTime:'2016-10-02 22:31'},
-  		{index:10,userName:'momo',nickName:'大漠穷秋',regTime:'2016-10-02 22:31',lastLoginTime:'2016-10-02 22:31'}
+  		{index:1,userId:1,userName:'momo',nickName:'大漠穷秋',regTime:'2016-10-02 22:31',lastLoginTime:'2016-10-02 22:31'},
+  		{index:2,userId:2,userName:'momo',nickName:'大漠穷秋',regTime:'2016-10-02 22:31',lastLoginTime:'2016-10-02 22:31'},
+  		{index:3,userId:3,userName:'momo',nickName:'大漠穷秋',regTime:'2016-10-02 22:31',lastLoginTime:'2016-10-02 22:31'},
+  		{index:4,userId:4,userName:'momo',nickName:'大漠穷秋',regTime:'2016-10-02 22:31',lastLoginTime:'2016-10-02 22:31'},
+  		{index:5,userId:5,userName:'momo',nickName:'大漠穷秋',regTime:'2016-10-02 22:31',lastLoginTime:'2016-10-02 22:31'},
+  		{index:6,userId:6,userName:'momo',nickName:'大漠穷秋',regTime:'2016-10-02 22:31',lastLoginTime:'2016-10-02 22:31'},
+  		{index:7,userId:7,userName:'momo',nickName:'大漠穷秋',regTime:'2016-10-02 22:31',lastLoginTime:'2016-10-02 22:31'},
+  		{index:8,userId:8,userName:'momo',nickName:'大漠穷秋',regTime:'2016-10-02 22:31',lastLoginTime:'2016-10-02 22:31'},
+  		{index:9,userId:9,userName:'momo',nickName:'大漠穷秋',regTime:'2016-10-02 22:31',lastLoginTime:'2016-10-02 22:31'},
+  		{index:10,userId:10,userName:'momo',nickName:'大漠穷秋',regTime:'2016-10-02 22:31',lastLoginTime:'2016-10-02 22:31'}
   	];
 
   	public totalItems:number = 64;
-	public currentPage:number = 4;
+	  public currentPage:number = 4;
 	 
-	public maxSize:number = 5;
-	public bigTotalItems:number = 175;
-	public bigCurrentPage:number = 1;
+	  public maxSize:number = 5;
+	  public bigTotalItems:number = 175;
+	  public bigCurrentPage:number = 1;
 	
-	constructor() { }
+	  constructor() { }
 
   	ngOnInit() {
+
   	}
 
-	public setPage(pageNo:number):void {
-	   this.currentPage = pageNo;
-	};
+	  public setPage(pageNo:number):void {
+	     this.currentPage = pageNo;
+	  };
 	 
-	public pageChanged(event:any):void {
-	  console.log('Page changed to: ' + event.page);
-	  console.log('Number items per page: ' + event.itemsPerPage);
-	};
+	  public pageChanged(event:any):void {
+      console.log(event);
+	  };
 
-  	getUserByType(typeId){
-  		console.log("Get users by type:"+typeId);
-  	}
+    //Search user
+    public searchUser(input){
+      console.log(input.value);
+    }
+
+    public resetSearch(input){
+      input.value="";
+    }
+
+    public delUser(userId){
+      console.log(userId);
+      let resultIndex=-1;
+      for(let i=0;i<this.users.length;i++){
+        let user=this.users[i];
+        if(user&&user.userId==userId){
+          resultIndex=i;
+          break;
+        }
+      }
+      if(resultIndex!=-1){
+        this.users.splice(resultIndex,1);
+      }
+    }
+
+    public userDetail(userId){
+      console.log(userId);
+    }
 }
