e962f3e89fdd38467eb052af5c4b3ff91b4535c1
zhang.xiaofei10 <zhang.xiaofei10@zte.com.cn>
2016-10-06 21:36:51 +0800

    using localstorage to sync data;

--- src/app/user/add-user/add-user.component.ts
+++ src/app/user/add-user/add-user.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-add-user',
@@ -6,11 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-user.component.scss']
 })
 export class AddUserComponent implements OnInit {
-	public userName:String;
-	public nickName:String;
-	public alerts:Array<Object> = [];
+	  public userName:String;
+	  public nickName:String;
+	  public alerts:Array<Object> = [];
 
-  	constructor() { }
+  	constructor(private router: Router,private route: ActivatedRoute) {
+
+    }
 
   	ngOnInit() {
   	}
@@ -18,8 +21,21 @@ export class AddUserComponent implements OnInit {
   	public submitForm(f){
   		if(f.form.valid){
   			console.log(f.form.value);
+        let newUser=f.form.value;
+        let users=[];
+        if(window.localStorage.getItem("users")){
+          users=JSON.parse(window.localStorage.getItem("users"));
+        }
+        newUser.userId=users.length+1;
+        users.push(newUser);
+        window.localStorage.setItem("users",JSON.stringify(users));
+
   			this.alerts.push({msg: 'Add User Success!', type: 'success', dismissOnTimeout:1000});
-  		}else{
+        let routerInstance=this.router;
+        setTimeout(function(){
+          routerInstance.navigateByUrl("/user");
+        },1000);
+      }else{
   			this.alerts.push({msg: 'Please check the input...', type: 'danger', dismissOnTimeout:1000});
   		}
   	}
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -8,31 +8,21 @@ import { PaginationModule } from 'ng2-bootstrap/ng2-bootstrap';
   styleUrls: ['./user.component.scss']
 })
 export class UserComponent implements OnInit {
-    //test data
-  	public users:any=[
-  		{index:1,userId:1,userName:'momo',nickName:'大漠穷秋',regTime:'2016-10-02 22:31',lastLoginTime:'2016-10-02 22:31'},
-  		{index:2,userId:2,userName:'momo',nickName:'大漠穷秋',regTime:'2016-10-02 22:31',lastLoginTime:'2016-10-02 22:31'},
-  		{index:3,userId:3,userName:'momo',nickName:'大漠穷秋',regTime:'2016-10-02 22:31',lastLoginTime:'2016-10-02 22:31'},
-  		{index:4,userId:4,userName:'momo',nickName:'大漠穷秋',regTime:'2016-10-02 22:31',lastLoginTime:'2016-10-02 22:31'},
-  		{index:5,userId:5,userName:'momo',nickName:'大漠穷秋',regTime:'2016-10-02 22:31',lastLoginTime:'2016-10-02 22:31'},
-  		{index:6,userId:6,userName:'momo',nickName:'大漠穷秋',regTime:'2016-10-02 22:31',lastLoginTime:'2016-10-02 22:31'},
-  		{index:7,userId:7,userName:'momo',nickName:'大漠穷秋',regTime:'2016-10-02 22:31',lastLoginTime:'2016-10-02 22:31'},
-  		{index:8,userId:8,userName:'momo',nickName:'大漠穷秋',regTime:'2016-10-02 22:31',lastLoginTime:'2016-10-02 22:31'},
-  		{index:9,userId:9,userName:'momo',nickName:'大漠穷秋',regTime:'2016-10-02 22:31',lastLoginTime:'2016-10-02 22:31'},
-  		{index:10,userId:10,userName:'momo',nickName:'大漠穷秋',regTime:'2016-10-02 22:31',lastLoginTime:'2016-10-02 22:31'}
-  	];
-
   	public totalItems:number = 64;
 	  public currentPage:number = 4;
 	 
 	  public maxSize:number = 5;
 	  public bigTotalItems:number = 175;
 	  public bigCurrentPage:number = 1;
-	
-	  constructor() { }
+	  public users:any;
 
-  	ngOnInit() {
+	  constructor() {
+      //Get test data from localStorage,data was defined in app.component.ts
+      this.users=JSON.parse(window.localStorage.getItem("users"));
+      console.log(this.users);
+    }
 
+    ngOnInit() {
   	}
 
 	  public setPage(pageNo:number):void {
@@ -64,6 +54,7 @@ export class UserComponent implements OnInit {
       }
       if(resultIndex!=-1){
         this.users.splice(resultIndex,1);
+        window.localStorage.setItem("users",JSON.stringify(this.users));
       }
     }
 
